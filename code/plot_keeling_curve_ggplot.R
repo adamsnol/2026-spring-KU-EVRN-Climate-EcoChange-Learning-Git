abc0f5c5ddeea39f633bff07a0f0d9b274469b36
Nolan Adams <n689a536@ku.edu>
2026-03-04 16:39:10 -0600

    Initial commit

---
+++ code/plot_keeling_curve_ggplot.R
@@ -0,0 +1,24 @@
+# an R script to use dplyr and ggplot to visualize
+# the keeling curve of Co2 concentrations
+
+# Nolan Adams
+# nolan.adams132@gmail.com
+# Feb 25th, 2026
+
+# first step is load the packages we want to use
+library("dplyr")
+library("ggplot2")
+
+#load in the csv data
+mlo_data <- read.csv("data/monthly_in_situ_co2_mlo_cleaned.csv")
+
+#use a dplyr pipe and filter function to get rid of -99 value
+mlo_data %>%
+  filter(CO2 > 0) %>%
+  filter(Yr > 1985) %>%
+  ggplot(aes(x = Date.1,
+             y = CO2)) +
+  geom_line( )
+
+ggsave("output/ggplot_kneeling.png")
+
